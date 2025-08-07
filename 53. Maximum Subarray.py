b5df4023b2a654096264701a16024fb2ebcf111d
Dhrubendu Das <140189756+dhrubendu2003@users.noreply.github.com>
2025-08-08 01:58:35 +0530

    Create 53. Maximum Subarray.py

---
+++ 53. Maximum Subarray.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        curr_sum = max_sum = nums[0]
+
+        for i in range(1, len(nums)):
+            curr_sum = max(curr_sum + nums[i], nums[i])
+            max_sum = max(curr_sum, max_sum)
+
+        return max_sum
